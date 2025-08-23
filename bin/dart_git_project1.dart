2be0998fe26e7e0951952efcddcf7853067763ee
YadaSangphech6631501030 <6631501030@lamduan.mfu.ac.th>
2025-08-23 10:53:14 +0700

    intial commit

---
+++ bin/dart_git_project1.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+// login
+void main() async {
+  print("\n===== Login =====");
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+
+  //Incomplete input
+  if (username == null || password == null) {
+    print("Incomplete input");
+    return;
+  }
+
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: body);
+
+  if (response.statusCode == 200) {
+    await expenseMenu(username);
+  } else if (response.statusCode == 401 || response.statusCode == 500) {
+    print(response.body); 
+  } else {
+    print("Unknown error");
+  }
+}
+
+Future<void> expenseMenu(String username) async {
+  while (true) {
+    print("\n===== Expense Tracking =====");
+    print("Welcome $username ");
+    print("1. All expenses");         // dev 1
+    print("2. Today's expenses");      // dev 1
+    print("3. Search expense");        // dev 2
+    print("4. Add new expense");       // dev 3
+    print("5. Delete an expense");     // dev 3
+    print("6. Exit"); 
+    stdout.write("Choose ... ");
+    String? choice = stdin.readLineSync()?.trim();
+
+    if (choice == '1') {
+      await showAllExpenses();
+    } else if (choice == '2') {
+      await showTodayExpenses();
+    } else if (choice == '3') {
+      await searchExpenses();
+    } else if (choice == '4') {
+      await addExpenses();
+    } else if (choice == '5') {
+      await deleteExpenses();
+    } else if (choice == '6') {
+      print("--- Bye ---");
+      break;
+    } else {
+      print("Invalid option.");
+    }
+  }
+}
+
+Future<void> showAllExpenses() async {
+ print("----- All expenses -----");
+ // Call API (GET /expenses)
+ // Parse JSON response
+ // Print all expenses 
+}
+
+Future<void> showTodayExpenses() async {
+ print("----- Today's expenses -----");
+  // Call API (GET /expenses/today)
+  // Filter expenses by today's date
+  // Print today's expenses
+}
+
+
+Future<void> searchExpenses() async {
+ print("----- Search expenses -----");
+   // Ask user for keyword/date
+  // Call API (GET /expenses/search?query=...)
+  // Print search results
+}
+
+Future<void> addExpenses() async {
+  print("===== Add new expenses =====");
+  // Ask user to enter expense name and amount
+  // Call API (POST /expenses)
+  // Confirm if the expense was added successfully
+}
+
+Future<void> deleteExpenses() async {
+  print("===== Delete expenses =====");
+   // Ask user for expense ID
+  // Call API (DELETE /expenses/:id)
+  // Confirm if the expense was deleted successfully
+}
---
+++ test/dart_git_project1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_git_project1/dart_git_project1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
